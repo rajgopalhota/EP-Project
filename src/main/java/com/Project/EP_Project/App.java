bd76e1c4b70bd89d94792981a3f395b1f39f9fde
rajgopalhota <rajgopalhota.com>
2023-06-30 22:16:16 +0530

    Ep initialized

---
+++ src/main/java/com/Project/EP_Project/App.java
@@ -0,0 +1,13 @@
+package com.Project.EP_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
